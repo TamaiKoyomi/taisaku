17da4a5046eba8f41afb3de8f1be4191aa934a24
TamaiKoyomi <168719766+TamaiKoyomi@users.noreply.github.com>
2025-01-28 08:49:00 +0000

    adsf

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,18 +14,18 @@ def factorize(n):
             return True
 
 st.title('そすー')
-a = int(st.number_input('a' , placeholder = "aの値を入力してください"))
+a = st.number_input('a' , placeholder = "aの値を入力してください")
 b = st.number_input('b' , placeholder = 'bの値を入力してください')
 
 #素数ならTrue,合成数ならFalseを返す
 x = factorize(3 * a + 2 * b)
 
-st.write(math.gcd(a , b))
+st.write(math.gcd(int(a) , int(b)))
 
 #a,bの条件の確認および例外処理
 #if math.gcd(a , b) != 1 or a != 0 and a % 2 == 0 or b != 0 and b % 3 == 0 or a % 5 == b % 5 and a != 1 and b != 1:
 #    x = False
 
-st.write('factorize:' + str(x))
+'''st.write('factorize:' + str(x))
 st.write('number:' + str(3 * a + 2 * b) , '' , 'a:' + str(a) , '' , 'b:' + str(b))
-st.write('gcd:' + str(math.gcd(a , b)) , '' * 2 , 'a.mod:' + str(a % 2) , '' , 'b.mod:' + str(b % 3))
+st.write('gcd:' + str(math.gcd(a , b)) , '' * 2 , 'a.mod:' + str(a % 2) , '' , 'b.mod:' + str(b % 3))'''
